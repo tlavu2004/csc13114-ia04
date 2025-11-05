f7c7353a45ae4c4b1203a5190ffa065752abefd7
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2025-11-05 12:38:28 +0700

    refactor: move UserModule to specific folder

--- backend/src/user/modules/user.module.ts
+++ backend/src/user/modules/user.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schemas/user.schema';
-import { UserService } from './services/user.service';
-import { UserController } from './controllers/user.controller';
+import { User, UserSchema } from '../schemas/user.schema';
+import { UserService } from '../services/user.service';
+import { UserController } from '../controllers/user.controller';
 
 @Module({
   imports: [
